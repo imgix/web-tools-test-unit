319d7763fbab99df6044cb5e0f0746ec53ae8307
Jason Eberle <jason@imgix.com>
2017-01-25 16:41:03 -0800

    Use main-bower-files to pull external dependencies

--- tools/task.test-unit.js
+++ tools/task.test-unit.js
@@ -1,50 +1,56 @@
 var _ = require('lodash'),
     merge = require('merge2'),
-    argFilter = require('web-tools/tools/misc/arg-filter'),
+    mainBowerFiles = require('main-bower-files'),
+    argFilter = require('web-tools/tools/misc/arg-filter.js'),
     unitTestPipeline = require('./pipeline.test-unit.js');
 
 module.exports = function setUpTask(gulp) {
   var testConfig = _.get(gulp, 'webToolsConfig.unitTests'),
-      appAssetDependencies,
-      extAssetDependencies,
+      assetDependencies,
       taskDependencies = [];
-
-  function prefixAssetType(prefix, assetType) {
-    var prefixed = prefix + '-' + assetType;
-    taskDependencies.push('build-' + prefixed);
-    return prefixed;
-  }
 
   if (!testConfig) {
     return;
   }
 
-  appAssetDependencies = _.map(testConfig.appAssetDependencies, _.partial(prefixAssetType, 'app'));
-  extAssetDependencies = _.map(testConfig.extAssetDependencies, _.partial(prefixAssetType, 'ext'));
+  assetDependencies = _.map(testConfig.appAssetDependencies, function addType(type) {
+    taskDependencies.push('build-app-' + type);
+    return 'app-' + type;
+  });
 
   gulp.task('test-unit', taskDependencies, function task() {
-    var assetDependencyStreams,
-        testStream;
+    var bowerFiles,
+        bowerStream,
+        assetStreams,
+        testStream,
+        allStreams;
 
-    assetDependencyStreams = _.chain(appAssetDependencies)
-      .concat(extAssetDependencies)
-      .map(function getStream(name) {
-          var stream = gulp.streamCache.get(name);
+    // Get Bower dev dependencies as well, since they can include test bootstraps
+    bowerFiles = mainBowerFiles({
+      includeDev: true,
+      paths: {
+          bowerJson: _.get(gulp, 'webToolsConfig.bower.json'),
+          bowerDirectory: _.get(gulp, 'webToolsConfig.bower.components')
+        },
+      filter: '**/*.js'
+    });
 
-          if (!!stream) {
-            assetDependencyStreams[name] = stream;
-          }
-        })
-      .compact()
-      .value();
+    bowerStream = gulp.src(bowerFiles, {read: false});
+
+    assetStreams = _.map(assetDependencies, function getStream(name) {
+      return gulp.streamCache.get(name);
+    });
 
     testStream = gulp.src(testConfig.testSrc, {read: false})
       .pipe(argFilter());
 
-    return merge(
-      assetDependencyStreams,
+    allStreams = merge.apply(null, _.flatten([
+      bowerStream,
+      assetStreams,
       testStream
-    ).pipe(unitTestPipeline(testConfig.karmaOptions));
+    ]));
+
+    return allStreams.pipe(unitTestPipeline(testConfig.karmaOptions));
   }, {
     description: 'Run unit tests with Karma.',
     category: 'test',
